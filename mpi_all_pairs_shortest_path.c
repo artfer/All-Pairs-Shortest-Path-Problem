f9c505fe02b5c2296ce49cc3a40e3e8759e4692a
artfer <artureco95.af@gmail.com>
2019-11-02 11:41:31 +0000

    Main function written. Aux functions missing.

--- mpi_all_pairs_shortest_path.c
+++ mpi_all_pairs_shortest_path.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-type_def struct {
+typedef struct {
   int p;              // Total number of processes
   MPI_Comm comm;      // Comunicator for entire grid
   MPI_Comm row_comm;  // Comunicator for my row
@@ -13,6 +13,25 @@ type_def struct {
   int my_rank;        // My rank in the grid communicator
 } GRID_INFO_TYPE;
 
+typedef struct {
+  
+} LOCAL_MATRIX_TYPE;
+
+typedef struct {
+  
+} DERIVED_LOCAL_MATRIX;
+
+
+void Setup_grid(GRID_INFO_TYPE* grid);
+void Fox(int n, GRID_INFO_TYPE* grid,
+    LOCAL_MATRIX_TYPE* local_A,
+    LOCAL_MATRIX_TYPE* local_B,
+    LOCAL_MATRIX_TYPE* local_C);
+void Local_matrix_multiply(LOCAL_MATRIX_TYPE* local_A,
+    LOCAL_MATRIX_TYPE* local_B,
+    LOCAL_MATRIX_TYPE* local_C);
+LOCAL_MATRIX_TYPE* Local_matrix_allocate(int n_bar);
+void Set_to_zero(LOCAL_MATRIX_TYPE* local_A);
 
 void Setup_grid(GRID_INFO_TYPE* grid){
   int old_rank;
@@ -51,6 +70,47 @@ void Setup_grid(GRID_INFO_TYPE* grid){
   MPI_Cart_sub(grid->comm,varying_coords,&(grid->col_comm));
 }
 
+
+void Fox(int n, GRID_INFO_TYPE* grid,
+    LOCAL_MATRIX_TYPE* local_A,
+    LOCAL_MATRIX_TYPE* local_B,
+    LOCAL_MATRIX_TYPE* local_C){
+  
+  LOCAL_MATRIX_TYPE* temp_A;
+  int step; 
+  int bcast_root;
+  int n_bar; // order of block submatrix = n/q
+  int source;
+  int dest;
+  int tag = 43;
+  MPI_Status status;
+
+  n_bar = n/grid->q;
+  Set_to_zero(local_C);
+
+  // Calculate addresses for circular shift of B
+  source = (grid->my_row + 1) % grid->q;
+  dest = (grid->my_row + grid->q - 1) % grid->q;
+
+  // Set aside storage for the broadcast block of A 
+  temp_A = Local_matrix_allocate(n_bar);
+
+  for(step = 0; step < grid->q; step++){
+    bcast_root = (grid->my_row + step) % grid->q;
+    if(bcast_root == grid->my_col) {
+      MPI_Bcast(local_A, 1, DERIVED_LOCAL_MATRIX,bcast_root, grid->row_comm);
+      Local_matrix_multiply(local_A,local_B,local_C);
+    } else {
+      MPI_Bcast(temp_A, 1, DERIVED_LOCAL_MATRIX, bcast_root, grid->row_comm);
+      Local_matrix_multiply(temp_A,local_B,local_C);
+    }
+    MPI_Send(local_B, 1, DERIVED_LOCAL_MATRIX, dest, tag, grid->col_comm);
+    MPI_Recv(local_B, 1, DERIVED_LOCAL_MATRIX, source, tag, grid->col_comm, &status);
+  } 
+}
+    
+
+
 void main(int argc, char **argv) {
   int numprocs, rank, tag, N;
   double start, end;
@@ -72,9 +132,6 @@ void main(int argc, char **argv) {
     }
 
   }
-
-
-
   MPI_Barrier(MPI_COMM_WORLD);
 
   MPI_Finalize();
