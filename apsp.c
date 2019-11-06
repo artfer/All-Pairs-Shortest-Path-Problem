18a07dc568b996f4c91c6b3715ad22ed4bb2520b
artfer <artureco95.af@gmail.com>
2019-11-06 04:28:08 +0000

    Finally it works!!

--- apsp.c
+++ apsp.c
@@ -5,8 +5,6 @@
 
 #define MAX 65536
 #define MIN(x, y) ((x) < (y)) ? (x) : (y)
-#define ZERO_TO_MAX(x) (x == 0  ) ? MAX : x
-#define MAX_TO_ZERO(x) (x == MAX) ?   0 : x 
 
 
 typedef struct {
@@ -20,95 +18,18 @@ typedef struct {
   int my_rank;        // My rank in the grid communicator
 } GRID_INFO_TYPE;
 
+void Setup_grid(GRID_INFO_TYPE* grid);
+int  Transform(int i, int j, int val);
+int  Transform_back(int val);
+int  Get_value(int* local_A, int i, int j, int n_bar);
+void Put_value(int* local_A, int i, int j, int n_bar, int val);
+void Update_value(int* local_A, int i, int j, int n_bar, int val); 
+void Read_matrix(int* matrix, GRID_INFO_TYPE* grid, int n, int q);
+void Print_matrix(int* matrix, GRID_INFO_TYPE* grid, int n, int q);
+void Fox(int n, GRID_INFO_TYPE* grid, int* local_A, int* local_B, int* local_C);
+void Local_matrix_multiply(int* local_A, int* local_B, int* local_C, int n_bar);
+void Set_to_max(int* local_A, int n_bar);
 
-
-void Read_matrix(int** a, GRID_INFO_TYPE* grid, int n) {
-
-    int        mat_row, mat_col;
-    int        grid_row, grid_col;
-    int        dest;
-    int        coords[2];
-    int tmp;
-    MPI_Status status;
-    
-    if (grid->my_rank == 0) {
-        int ** tmp;
-        for (mat_row = 0;  mat_row < n; mat_row++) {
-            grid_row = mat_row/grid->q;
-            coords[0] = grid_row;
-            for (grid_col = 0; grid_col < grid->q; grid_col++) {
-                coords[1] = grid_col;
-                MPI_Cart_rank(grid->comm, coords, &dest);
-                if (dest == 0) {
-                    for (mat_col = 0; mat_col < grid->q; mat_col++){
-                        scanf("%d",tmp);
-                        tmp = ZERO_TO_MAX(tmp);
-                        a[mat_row][mat_col] = tmp;
-                        //scanf("%f", (local_A->entries)+mat_row*Order(local_A)+mat_col);
-                    }
-                } else {
-                    for(mat_col = 0; mat_col < grid->q; mat_col++){
-                        scanf("%d",tmp);
-                        tmp = ZERO_TO_MAX(tmp);
-                        MPI_Send(tmp, 1, MPI_INT, dest, 0,grid->comm);
-                    }
-                }
-            }
-        }
-    } else {  
-        for (mat_row = 0; mat_row < grid->q; mat_row++) 
-            for (mat_col = 0; mat_row < grid->q; mat_col++) 
-                MPI_Recv(&a[mat_row][mat_col], 1, MPI_INT, 0, 0, grid->comm, &status);
-    }          
-} 
-
-void Print_matrix(
-         int** local_A, GRID_INFO_TYPE* grid, int n) {
-    int mat_row, mat_col;
-    int grid_row, grid_col;
-    int source;
-    int coords[2];
-    int tmp;
-    MPI_Status status;
-
-    if (grid->my_rank == 0) {
-        for (mat_row = 0;  mat_row < n; mat_row++) {
-            grid_row = mat_row/grid->q;
-            coords[0] = grid_row;
-            for (grid_col = 0; grid_col < grid->q; grid_col++) {
-                coords[1] = grid_col;
-                MPI_Cart_rank(grid->comm, coords, &source);
-                if (source == 0) {
-                    for(mat_col = 0; mat_col < grid->q; mat_col++)
-                        printf("%d ", local_A[mat_row][mat_col]);
-                } else {
-                    
-                    for(mat_col = 0; mat_col < grid->q; mat_col++)
-                        MPI_Recv(tmp, 1, MPI_INT, source, 0, grid->comm, &status);
-                        printf("%d ", tmp);
-                }
-            }
-            printf("\n");
-        }
-    } else {
-        for (mat_row = 0; mat_row < grid->q; mat_row++)
-            for (mat_row = 0; mat_row < grid->q; mat_col++)  
-            MPI_Send(local_A[mat_row][mat_col], 1, MPI_INT, 0, 0, grid->comm);
-    }
-                     
-}
-
-
-void Local_matrix_multiply(int** a, int** b, int** c,int q) {
-    int i, j, k, x;
-
-    for (i = 0; i < q; i++)
-        for (j = 0; j < q; j++)
-            for (k = 0; k < q; k++){
-                x = a[i][k] + b[k][j];
-                c[i][j] = MIN(c,x);
-            }
-}
 
 void Setup_grid(GRID_INFO_TYPE* grid){
   int old_rank;
@@ -145,13 +66,93 @@ void Setup_grid(GRID_INFO_TYPE* grid){
   varying_coords[0] = 1;
   varying_coords[1] = 0;
   MPI_Cart_sub(grid->comm,varying_coords,&(grid->col_comm));
+
+  MPI_Barrier(MPI_COMM_WORLD);
 }
 
 
+int Transform(int i, int j, int val){
+    if(i == j)
+        return 0;
+    if(val == 0)
+        return MAX;
+    return val;
+}
+
+int Transform_back(int val){
+    return val == MAX ? 0 : val;
+}
+
+int Get_value(int* local_A, int i, int j, int n_bar){
+    int offset = (i * n_bar) + j;
+    return local_A[offset];
+}
+
+void Put_value(int* local_A, int i, int j, int n_bar, int val){
+    int offset = (i * n_bar) + j;
+    local_A[offset] = val;
+}
+
+void Update_value(int* local_A, int i, int j, int n_bar, int val){
+    int offset = (i * n_bar) + j;
+    local_A[offset] = MIN(local_A[offset],val);
+}
+
+
+void Read_matrix(int* matrix, GRID_INFO_TYPE* grid, int n, int q){
+    
+    int n_bar = n / q;
+    int src=0,dest;
+    int tmp;
+    int grid_coords[2];
+    MPI_Status status;
+
+    // reads the matrix and sends to the other processes
+    if(grid->my_rank == 0) {
+        
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j < n; j++){
+                    
+                    scanf("%d",&tmp);
+
+                    // to avoid zeros in the final result 
+                    tmp = Transform(i,j,tmp); 
+                                
+                    Put_value(matrix,i,j,n_bar,tmp);
+                }
+            }
+        }
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+void Print_matrix(int* matrix, GRID_INFO_TYPE* grid, int n, int q){
+    int n_bar = n / q;
+    int src,dest=0;
+    int tmp;
+    int grid_coords[2];
+    MPI_Status status;
+
+    // reads the matrix and sends to the other processes
+    if(grid->my_rank == 0) {
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                tmp = Get_value(matrix,i,j,n_bar);
+                tmp = Transform_back(tmp);
+                printf("%d ",tmp);
+            }
+            printf("\n");
+        }
+    }
+}
+
+
+
 void Fox(int n, GRID_INFO_TYPE* grid,
-    int** local_A,
-    int** local_B,
-    int** local_C){
+    int* local_A,
+    int* local_B,
+    int* local_C){
   
   int step; 
   int bcast_root;
@@ -162,30 +163,55 @@ void Fox(int n, GRID_INFO_TYPE* grid,
   MPI_Status status;
 
   n_bar = n/grid->q;
-  Set_to_zero(local_C);
+  Set_to_max(local_C, n_bar);
 
   // Calculate addresses for circular shift of B
   source = (grid->my_row + 1) % grid->q;
   dest = (grid->my_row + grid->q - 1) % grid->q;
 
   // Set aside storage for the broadcast block of A 
-  //temp_A = Local_matrix_allocate(n_bar);
-  int** tmp;
+  int* temp_A = (int*) malloc(n_bar * n_bar * sizeof(int));
 
   for(step = 0; step < grid->q; step++){
     bcast_root = (grid->my_row + step) % grid->q;
     if(bcast_root == grid->my_col) {
-      MPI_Bcast(local_A, 1, MPI_INT,bcast_root, grid->row_comm);
-      Local_matrix_multiply(local_A,local_B,local_C,grid->q);
+      MPI_Bcast(local_A, n_bar * n_bar, MPI_INT,bcast_root, grid->row_comm);
+      Local_matrix_multiply(local_A,local_B,local_C, n_bar);
     } else {
-      MPI_Bcast(tmp, 1, MPI_INT, bcast_root, grid->row_comm);
-      Local_matrix_multiply(tmp,local_B,local_C,grid->q);
+      MPI_Bcast(temp_A, n_bar * n_bar, MPI_INT, bcast_root, grid->row_comm);
+      Local_matrix_multiply(temp_A,local_B,local_C, n_bar);
     }
-    MPI_Send(local_B, 1, MPI_INT, dest, tag, grid->col_comm);
-    MPI_Recv(local_B, 1, MPI_INT, source, tag, grid->col_comm, &status);
+    MPI_Send(local_B, n_bar * n_bar, MPI_INT, dest, tag, grid->col_comm);
+    MPI_Recv(local_B, n_bar * n_bar, MPI_INT, source, tag, grid->col_comm, &status);
   } 
 }
 
+void Set_to_max(int* local_A, int n_bar){
+    for(int i = 0; i < n_bar; i++){
+        for(int j = 0; j < n_bar; j++){
+        Put_value(local_A,i,j,n_bar,MAX);
+        }
+    }
+
+}
+
+void Local_matrix_multiply(int* local_A, int* local_B, int* local_C, int n_bar){
+    
+    int tmp; 
+    for(int i = 0; i < n_bar; i++){
+        for(int j = 0; j < n_bar; j++){
+            for(int k = 0; k < n_bar; k++){
+                tmp = Get_value(local_A,i,k,n_bar) + 
+                      Get_value(local_B,k,j,n_bar);
+                //printf("%d ",tmp);
+                Update_value(local_C, i, j, n_bar, tmp);     
+            }
+            //printf("%d ",Get_value(local_C, i, j, n_bar));
+        
+        }
+        //printf("\n");
+    }
+}
 
 
 void main(int argc, char **argv) {
@@ -199,19 +225,30 @@ void main(int argc, char **argv) {
   Setup_grid(&grid);
   if (rank == 0)
     scanf("%d", &n);
-
   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  n_bar = n/grid.q;
+  
+  n_bar = n / grid.q;
+  
+  int* matrix; 
+  if (rank==0) matrix = (int *)malloc(n * n * sizeof(int));
 
+  int* local_A = (int *)malloc(n_bar * n_bar * sizeof(int));
+  int* local_C = (int *)malloc(n_bar * n_bar * sizeof(int));
 
-  int local_A[grid.q][grid.q];
-  int local_C[grid.q][grid.q];
-  Read_matrix(local_A, &grid, n);
-  //tmp = Local_matrix_allocate(n_bar);
+  Read_matrix(matrix,local_A, &grid, n, grid.q);
+  int f=2;
+  start = MPI_Wtime();
+  do{
+    MPI_Scatter(matrix,n_bar*n_bar,MPI_INT,local_A,n_bar*n_bar,MPI_INT,0,grid.comm);
+    Fox(n, &grid, local_A, local_A, local_C);
+    MPI_Gather(local_C, n_bar * n_bar, MPI_INT, matrix, n_bar * n_bar, MPI_INT, 0, grid.comm);
+    f+=f;
+  } while(f<n);
 
-  Fox(n, &grid, local_A, local_A, local_C);
-
-  Print_matrix(local_C, &grid, n);
+  end = MPI_Wtime();
+  double total = end - start;
+  printf("Time %.7f\n",total);
+  Print_matrix(matrix,local_C, &grid, n, grid.q);
 
   MPI_Finalize();
   return;
